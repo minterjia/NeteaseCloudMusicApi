126c35de85273c7507bd82eb55abbe15440744b2
米尔嘉 <61584074+minterjia@users.noreply.github.com>
2023-05-15 10:26:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ async function start() {
 }
 start()
 
+const express = require('express');
+const cors = require('cors');
+const apiRoutes = require('./routes');  // 导入API路由
+
+const app = express();
+
+// 设置CORS中间件
+app.use(cors({
+  origin: 'https://music.minterjia.com' // 允许访问的域名，可以是字符串或字符串数组
+}));
+
+// 添加API路由
+app.use('/', apiRoutes);
+
+// 启动API服务
+app.listen(3000, () => {
+  console.log('API服务已启动');
+});
